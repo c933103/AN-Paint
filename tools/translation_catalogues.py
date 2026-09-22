bfd3a5a049fe0ae0fe76713d2789ac50dcdb4882
c933103 <c933103@gmail.com>
2026-09-22 11:50:52 +0800

    Validate only actual positional Android format tokens

--- tools/translation_catalogues.py
+++ tools/translation_catalogues.py
@@ -17,7 +17,7 @@ DATA = ROOT / "translations"
 RES = ROOT / "Paintroid/src/main/res"
 
 FORMAT_TOKEN = re.compile(
-    r"%(?:\d+\$)?[-+# 0,(<]*(?:\d+)?(?:\.\d+)?[bBhHsScCdoxXeEfgGaAtT%n]"
+    r"(?:%\d+\$[-+# 0,(<]*(?:\d+)?(?:\.\d+)?[bBhHsScCdoxXeEfgGaAtT%n]|%%)"
 )
 
 
